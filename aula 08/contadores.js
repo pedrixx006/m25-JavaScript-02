66b47192c516c79cae78ed4c1f7f54d395cec489
pedro cintra <pedrixxxx008@gmail.com>
2025-10-25 09:41:17 -0300

    atividade

---
+++ aula 08/contadores.js
@@ -0,0 +1,17 @@
+/**
+ * atividade pratica de Javascript Intermediário
+ */
+for (let i=0;i<10;i++){
+    console.log("valor de i=" + i);
+}
+console.log("fim do for" );let i=1;
+
+while (i<10){
+    console.log("valor de i=" + i++);
+}
+i = 0 
+console.log("iniciando do while")
+do {
+    console.log("valor de i=" + i++);
+}while(i<10);
+console.log("fim do laço do..while")
--- aula 08/funcoes.js
+++ aula 08/funcoes.js
@@ -2,7 +2,6 @@
  * criação de uso de funções
  */
 //definindo função para usar
-
 function fatorial(numero){
     let ret = 1;
     //aqui abaixo será um laço decrescente 5 * 4 * 3 * 2 
@@ -13,3 +12,13 @@ return ret; //retomando valor
 }
 //usando função
 console.log("o fatorial de 5 é " + fatorial(5));
+//arrow function
+const fatorial_arrow = (numero) => {
+    let ret = 1;
+    for (let i=numero;i>1;i--){
+        ret *= i
+    }
+    return ret;
+}
+//usando a funcao
+console.log("o fatorial (arrow) de 5 é " + fatorial_arrow(5));
