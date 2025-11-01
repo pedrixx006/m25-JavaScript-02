af815c22ee871b0bfb2d8f7fb9036d487631c340
pedro cintra <pedrixxxx008@gmail.com>
2025-11-01 12:53:07 -0300

    aula 10

---
+++ aula 10/beckend/frontend/index.js
@@ -0,0 +1,28 @@
+const carregar = () => {
+    const tbody = document.querySelector("#id_dados");
+    axios.get("http://localhost:3000/produtos")
+        .then((ret) => {
+            ret.data.forEach((item, idx) => {
+                const tr = document.createElement('tr');
+                const td_id = document.createElement('td');
+                const td_desc = document.createElement('td');
+                const td_saldo = document.createElement('td');
+                const td_preco = document.createElement('td');
+                //adiciionando os tds nos trs
+                tr.appendChild(td_id);
+                tr.appendChild(td_desc);
+                tr.appendChild(td_saldo);
+                tr.appendChild(td_preco);
+                //adicionando ao tbody
+                tbody.appendChild(tr);
+                //colocando os valores nas tds
+                td_id.textContent = item.id;
+                td_desc.textContent = item.descricao;
+                td_saldo.textContent = item.saldo;
+                td_preco.textContent = item.preco
+            })
+
+        })
+}
+
+carregar();
